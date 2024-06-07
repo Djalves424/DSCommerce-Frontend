1717e5284d0b793337f32b9360257ab6eba0b83e
Djalves424 <djalves424@gmail.com>
2024-06-07 19:41:29 -0300

    Rendering first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import computerImg from './assets/computer.png';
-import cartImg from './assets/cart.svg';
 import "./App.css";
+import computerImg from "./assets/computer.png";
+import HeaderClient from './components/HeaderClient';
 
-function App() {
+export default function App() {
   return (
     <>
-      <header className="dsc-header-client">
-        <nav className="dsc-container">
-          <h1>DSCommerce</h1>
-          <div className="dsc-navbar-right">
-            <div className="dsc-menu-items-container">
-              <div className="dsc-menu-item">
-                <img src={cartImg} alt="Carrinho de compras" />
-              </div>
-            </div>
-            <a href="#">Entrar</a>
-          </div>
-        </nav>
-      </header>
+      <HeaderClient />
       <main>
         <section id="product-details-section" className="dsc-container">
           <div className="dsc-card dsc-mb20">
@@ -51,5 +39,3 @@ function App() {
     </>
   );
 }
-
-export default App;
