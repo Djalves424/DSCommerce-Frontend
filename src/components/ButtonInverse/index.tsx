567201291758b6a752bda907f1fe46bb519ca7f0
Djalves424 <djalves424@gmail.com>
2024-06-08 17:34:20 -0300

    Creating new components

---
+++ src/components/ButtonInverse/index.tsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+export default function ButtonInverse() {
+  return <div className="dsc-btn dsc-btn-white">
+    Início
+  </div>;
+}
---
+++ src/components/ProductCategory/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export default function ProductCategory() {
+    return (
+        <div className="dsc-category">
+            Eletrônicos
+        </div>
+    );
+}
