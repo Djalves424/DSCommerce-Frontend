823c9b2b8287fe27567eaa530d9a09f03c1d4aaa
Djalves424 <djalves424@gmail.com>
2024-06-14 16:21:45 -0300

    Passing a product object as Prop

--- src/components/ProductCategory/index.tsx
+++ src/components/ProductCategory/index.tsx
@@ -1,9 +1,13 @@
 import './styles.css';
 
-export default function ProductCategory() {
+type Props = {
+    name: string;
+}
+
+export default function ProductCategory({name }: Props) {
     return (
         <div className="dsc-category">
-            Eletrônicos
+            {name}
         </div>
     );
 }
--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -1,8 +1,27 @@
+import "./styles.css";
 import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
-import './styles.css';
+import { ProductDTO } from "../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Esta Tv é muito bonita",
+  imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletrônicos",
+    },
+    {
+      id: 3,
+      name: "Computadores",
+    },
+  ],
+};
 
 export default function ProductDetails() {
   return (
@@ -10,7 +29,7 @@ export default function ProductDetails() {
       <HeaderClient />
       <main>
         <section id="product-details-section" className="dsc-container">
-          <ProductDetailsCard />
+          <ProductDetailsCard product={product} />
           <div className="dsc-btn-page-container">
             <ButtonPrimary />
             <ButtonInverse />
