47ff044d1bb2651932531f8dbca2d6f2f41f845a
Djalves424 <djalves424@gmail.com>
2024-06-18 19:25:54 -0300

    Adding React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-//import ProductDetails from "./routes/ProductDetails";
-import Catalog from "./routes/Catalog";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ClientHome from "./routes/ClientHome";
+import Catalog from "./routes/ClientHome/Catalog";
+import ProductDetails from "./routes/ClientHome/ProductDetails";
 
 export default function App() {
   return (
-//<ProductDetails />
-<Catalog/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ClientHome />}>
+          <Route index element={<Catalog />} />
+          <Route path="Catalog" element={<Catalog />} />
+          <Route path="product-details" element={<ProductDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -0,0 +1,42 @@
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import { ProductDTO } from "../../../models/product";
+import "./styles.css";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Esta Tv é muito bonita",
+  imgUrl:
+    "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletrônicos",
+    },
+    {
+      id: 3,
+      name: "Computadores",
+    },
+    {
+      id: 4,
+      name: "Importados",
+    },
+  ],
+};
+
+export default function ProductDetails() {
+  return (
+    <main>
+      <section id="product-details-section" className="dsc-container">
+        <ProductDetailsCard product={product} />
+        <div className="dsc-btn-page-container">
+          <ButtonPrimary text="Comprar" />
+          <ButtonInverse text="Início" />
+        </div>
+      </section>
+    </main>
+  );
+}
