4ce28b547f823f473d05860fcaefa99972744a3b
Djalves424 <djalves424@gmail.com>
2024-07-04 16:39:58 -0300

    Cart mock data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientHome from "./routes/ClientHome";
 import Catalog from "./routes/ClientHome/Catalog";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
+import Cart from "./routes/ClientHome/Cart";
 
 export default function App() {
   return (
@@ -10,8 +11,8 @@ export default function App() {
         <Route path="/" element={<ClientHome />}>
           <Route index element={<Catalog />} />
           <Route path="Catalog" element={<Catalog />} />
-          <Route
-            path="product-details/:productId"element={<ProductDetails />} />
+          <Route path="product-details/:productId"element={<ProductDetails />} />
+          <Route path="Cart" element={<Cart />} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
