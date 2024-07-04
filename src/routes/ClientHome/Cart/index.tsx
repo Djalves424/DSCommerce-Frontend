184e6904b387b93478a61f3f0745f5e786bd6b54
Djalves424 <djalves424@gmail.com>
2024-07-04 18:59:15 -0300

    Saving an OrderDTO object to localStorage

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,33 +1,43 @@
 import "./styles.css";
-const cart = {
-  items: [
-    {
-      productId: 4,
-      quantity: 1,
-      name: "PC Gamer",
-      price: 1200,
-      imgUrl:
-        "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg",
-    },
-    {
-      productId: 5,
-      quantity: 2,
-      name: "Rails for Dummies",
-      price: 100.99,
-      imgUrl:
-        "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg",
-    },
-  ],
-};
+import { useEffect } from "react";
+import * as cartService from "../../../services/cart-service";
+import { OrderDTO, OrderItemDTO } from "../../../models/order";
+
+const item1: OrderItemDTO = new OrderItemDTO(
+  4,
+  1,
+  "PC Gamer",
+  1200,
+  "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"
+);
+
+const item2: OrderItemDTO = new OrderItemDTO(
+  5,
+  2,
+  "Rails for Dummies",
+  100.99,
+  "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg"
+);
 
 export default function Cart() {
+  const cart: OrderDTO = new OrderDTO();
+
+  useEffect(() => {
+    cart.items.push(item1);
+    cart.items.push(item2);
+
+    cartService.saveCart(cart);
+  }, []);
+
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
         <div className="dsc-card dsc-mb20">
-          {
-          cart.items.map((item) => (
-            <div  key={item.productId}className="dsc-cart-item-container dsc-line-bottom">
+          {cart.items.map((item) => (
+            <div
+              key={item.productId}
+              className="dsc-cart-item-container dsc-line-bottom"
+            >
               <div className="dsc-cart-item-left">
                 <img src={item.imgUrl} alt={item.name} />
                 <div className="dsc-cart-item-description">
---
+++ src/services/cart-service.ts
@@ -0,0 +1,7 @@
+import { OrderDTO } from "../models/order";
+import * as cartRepository from '../localstorage/cart-repository';
+
+
+export function saveCart(cart: OrderDTO) {
+    cartRepository.save(cart);
+}
