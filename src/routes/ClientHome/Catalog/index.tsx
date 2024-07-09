bae71449dda2b60be3b0609e03015454fef53c9f
Djalves424 <djalves424@gmail.com>
2024-07-09 15:47:29 -0300

    Searchbar onSearch event

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -7,20 +7,27 @@ import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 
 export default function Catalog() {
-  const [products, setProducts] = useState<ProductDTO[]>([]);
+  
+    const [products, setProducts] = useState<ProductDTO[]>([]);
+  
+    const [productName, setProductName] = useState("");
 
   useEffect(() => {
-    productService.findAll()
+    productService.findPageRequest(0, productName)
     .then(response => {
       setProducts(response.data.content);
     });
 
-  }, []);
+  }, [productName]);
+
+  function handleSearch(searchText: string) {
+    setProductName(searchText)
+  }
 
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {products.map((product) => (
