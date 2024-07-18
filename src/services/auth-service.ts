e14a5e1ca4185bdc9bebe195ab8aba838b432c50
Djalves424 <djalves424@gmail.com>
2024-07-18 18:33:03 -0300

    hasAnyRoles

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,5 +1,5 @@
 import QueryString from "qs";
-import { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO, RoleEnum } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
@@ -53,6 +53,22 @@ export function isAuthenticated(): boolean {
     const tokenPayload = getAccessTokenPayload();
     if (tokenPayload && tokenPayload.exp * 1000 > Date.now()) {
         return true;
+    }
+    return false;
+}
+
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+    if (roles.length === 0) {
+        return true;
+    }
+    const tokenPayload = getAccessTokenPayload();
+    if (tokenPayload !== undefined) {
+        for (var i = 0; i < roles.length; i++) {
+            if (tokenPayload.authorities.includes(roles[i])) {
+                return true;
+            }
+        }
+        //return roles.some(role => tokenData.authorities.includes(role));
     }
     return false;
 }
