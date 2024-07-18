d537f14cdae2251892c072a6187a1602eb9f4f36
Djalves424 <djalves424@gmail.com>
2024-07-18 17:22:56 -0300

    PrivateRoute

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Admin from "./routes/Admin";
 import AdminHome from "./routes/Admin/AdminHome";
 import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
 import { history } from "./utils/history";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -30,7 +31,7 @@ export default function App() {
             <Route path="Cart" element={<Cart />} />
             <Route path="Login" element={<Login />} />
           </Route>
-          <Route path="/admin/" element={<Admin />}>
+          <Route path="/admin/" element={<PrivateRoute><Admin /></PrivateRoute>}>
             <Route index element={<AdminHome />} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,7 +5,6 @@ import SearchBar from "../../../components/SearchBar";
 import * as productService from "../../../services/product-service";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
-import { isAuthenticated } from "../../../services/auth-service";
 
 type QueryParams = {
   page: number;
@@ -23,8 +22,6 @@ export default function Catalog() {
   });
 
   useEffect(() => {
-    console.log("AUTENTICADO", isAuthenticated());
-
     productService
       .findPageRequest(queryParams.page, queryParams.name)
       .then((response) => {
