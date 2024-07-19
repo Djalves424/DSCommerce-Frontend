036940432cee93f9b4c4247379e154b682019a93
Djalves424 <djalves424@gmail.com>
2024-07-19 16:37:23 -0300

    LoggedUser

---
+++ src/components/LoggedUser/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import * as authService from "../../services/auth-service";
+import { useContext } from "react";
+import { ContextToken } from "../../utils/context-token";
+
+export default function LoggedUser() {
+  const { contextTokenPayload, setContextTokenPayload } =
+    useContext(ContextToken);
+
+  function handleLogoutClick() {
+    authService.logout();
+    setContextTokenPayload(undefined);
+  }
+
+  return contextTokenPayload && authService.isAuthenticated() ? (
+    <div className="dsc-logged-user">
+      <p>{contextTokenPayload.user_name}</p>
+      <span onClick={handleLogoutClick}>Sair</span>
+    </div>
+  ) : (
+    <Link to="/login">Entrar</Link>
+  );
+}
