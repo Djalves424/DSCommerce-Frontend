7f5efd3b51c5e92777ca16982b53fc6a180b2d36
Djalves424 <djalves424@gmail.com>
2024-07-29 17:13:38 -0300

    React Select styling

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -8,6 +8,7 @@ import * as categoryService from "../../../services/category-service";
 import FormTextArea from "../../../components/FormITextArea";
 import { CategoryDTO } from "../../../models/category";
 import FormSelect from "../../../components/FormSelect";
+import { selectStyles } from "../../../utils/select";
 
 export default function ProductForm() {
   const params = useParams();
@@ -134,7 +135,8 @@ export default function ProductForm() {
               <div>
                 <FormSelect
                   {...formData.categories}
-                  className="dsc-form-control"
+                  className="dsc-form-control dsc-form-select-container"
+                  styles={selectStyles}
                   options={categories}
                   onChange={(obj: any) => {
                     const newFormData = forms.updateAndValidate(formData, "categories", obj);                   
