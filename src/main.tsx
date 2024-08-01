75ae2637b6e9f45f7b1280b66df999394238764c
Djalves424 <djalves424@gmail.com>
2024-08-01 15:24:51 -0300

    Deploy ready

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+window.React = React;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
